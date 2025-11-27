1e60ca86758a1384404a3fc4820a02f36f291f51
Iaroslav Babin <babin.y.v@gmail.com>
2025-11-27 20:19:54 +0300

    [TEST] Add base model tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,25 @@ def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+def test_score_low_risk():
+    data = {
+        "age": 25,
+        "income": 70000,
+        "months_on_book": 6,
+        "credit_limit": 12000
+    }
+    response = client.post("/score/", json=data)
+    assert response.status_code == 200
+    assert response.json()["prediction"] == "low_risk"
+
+def test_score_high_risk():
+    data = {
+        "age": 30,
+        "income": 25000,
+        "months_on_book": 4,
+        "credit_limit": 5000
+    }
+    response = client.post("/score/", json=data)
+    assert response.status_code == 200
+    assert response.json()["prediction"] == "high_risk"
